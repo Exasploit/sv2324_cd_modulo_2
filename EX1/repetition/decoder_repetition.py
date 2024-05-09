7262e7d759bdc0cfa13eca77b7e968e325e170a9
Exasploit <44811@alunos.isel.ipl.pt>
2024-05-09 16:02:06 +0100

    added decoder to its apropriate place

--- EX1/repetition/decoder_repetition.py
+++ EX1/repetition/decoder_repetition.py
@@ -9,3 +9,12 @@ def decoder_repetition(data, n):
         else:
             decoded_data += '0'  # More 0s or equal number of 1s and 0s
     return decoded_data
+
+def decoder_efficient_repetition(in_sequence, n):
+    # if n is even, the majority of bits is n/2
+    # if n is odd, the majority of bits is n//2 + 1
+    # skip ahead n bits at a time and count the number of 1s
+    out_sequence = ''
+    for i in range(0, len(in_sequence), n):
+        out_sequence += '1' if in_sequence[i:i + n].count('1') > n // 2 else '0'
+    return out_sequence
--- EX1/repetition/encoder_repetition.py
+++ EX1/repetition/encoder_repetition.py
@@ -9,11 +9,3 @@ def encoder_repetition(in_sequence, n):
     return out_sequence
 
 
-def decoder_efficient_repetition(in_sequence, n):
-    # if n is even, the majority of bits is n/2
-    # if n is odd, the majority of bits is n//2 + 1
-    # skip ahead n bits at a time and count the number of 1s
-    out_sequence = ''
-    for i in range(0, len(in_sequence), n):
-        out_sequence += '1' if in_sequence[i:i + n].count('1') > n // 2 else '0'
-    return out_sequence
