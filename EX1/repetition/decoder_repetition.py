be785982e346c5e889fdb53e585e87a3fd3c1ce1
Exasploit <44811@alunos.isel.ipl.pt>
2024-05-09 15:16:23 +0100

    decoder repetition done

--- EX1/repetition/decoder_repetition.py
+++ EX1/repetition/decoder_repetition.py
@@ -1,6 +1,13 @@
 
 
 
-def decoder_repetition():
-
-
+# Defining the decoding function for repetition code
+def decode(data, n):
+    decoded_data = ''
+    for i in range(0, len(data), n):
+        chunk = data[i:i+n]
+        if chunk.count('1') > chunk.count('0'):
+            decoded_data += '1'  # More 1s than 0s
+        else:
+            decoded_data += '0'  # More 0s or equal number of 1s and 0s
+    return decoded_data
