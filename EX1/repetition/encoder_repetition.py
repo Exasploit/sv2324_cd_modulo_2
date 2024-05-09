d65428f917840e0d8e8615a498d092aaa65cea63
Exasploit <44811@alunos.isel.ipl.pt>
2024-05-09 15:56:25 +0100

    addded more efficient reptition decoder

--- EX1/repetition/encoder_repetition.py
+++ EX1/repetition/encoder_repetition.py
@@ -7,3 +7,13 @@ def encoder_repetition(in_sequence, n):
     for bit in in_sequence:
         out_sequence += bit * n
     return out_sequence
+
+
+def decoder_efficient_repetition(in_sequence, n):
+    # if n is even, the majority of bits is n/2
+    # if n is odd, the majority of bits is n//2 + 1
+    # skip ahead n bits at a time and count the number of 1s
+    out_sequence = ''
+    for i in range(0, len(in_sequence), n):
+        out_sequence += '1' if in_sequence[i:i + n].count('1') > n // 2 else '0'
+    return out_sequence
